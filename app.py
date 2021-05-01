dcc3fc38c4e8fe3b8fad03b251ec7256796d787d
tiffanyharris711 <tiffanyaharris711@gmail.com>
2021-05-01 14:08:10 -0500

    working copy

--- app.py
+++ app.py
@@ -6,49 +6,36 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
-#################################################
-# Database Setup
-#################################################
 engine = create_engine("sqlite:///pokemonGo.sqlite")
 
-# reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+
 Base.prepare(engine, reflect=True)
 
-# Save references to each table
 Pokemon_Names_Table = Base.classes.tablename
 
-
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
 
 
-#################################################
-# Flask Routes
-#################################################
-
-# @app.route("/")
-# def welcome():
-#     return (
-#         f"Welcome to the Pokemon Go Analysis API!<br/>"
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/names<br/>"
-#     )
-
+@app.route("/")
+def home():
+    print('Hello')
+    return render_template("index.html")
 
 @app.route("/api/v1.0/names")
 def names():
     """Return a list of Pokemon Go Names"""
-    # Create our session (link) from Python to the DB
+    
     session = Session(engine)
     
-    results = session.query(Pokemon_Names_Table.id).all()
+    results = session.query(Pokemon_Names_Table.id, Pokemon_Names_Table.pokename).all()
 
     # all_names = list(np.ravel(results))
     return jsonify(results)
+
+
+if __name__ == '__main__':
+    app.run()
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,17 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://pokeapi.co/api/v2/ability/1";
+// var queryUrl = "https://pokeapi.co/api/v2/ability/1";
 
+// $.get(queryUrl)
+//     .done(data => {
+//         console.log(data);
+//         // const parseData = JSON.parse(data);
+//         console.log(data.effect_entries[1].effect);
+//     });
+
+var queryUrl = '/api/v1.0/names'
 $.get(queryUrl)
     .done(data => {
         console.log(data);
-        // const parseData = JSON.parse(data);
-        console.log(data.effect_entries[1].effect);
     });
 
 // // get table references

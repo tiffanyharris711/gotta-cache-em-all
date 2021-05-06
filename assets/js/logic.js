d31f16aac77e916093e76e00502e79d8587ae0ba
Bao <baodphm@gmail.com>
2021-05-06 00:46:14 -0500

    Layout Changes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,39 +1,46 @@
-// Store our API endpoint inside queryUrl
-// var queryUrl = "https://pokeapi.co/api/v2/ability/1";
+var queryUrl = '/api/v1/names'
 
-// $.get(queryUrl)
-//     .done(data => {
-//         console.log(data);
-//         // const parseData = JSON.parse(data);
-//         console.log(data.effect_entries[1].effect);
-//     });
+function init() {
 
-// var queryUrl = '/api/v1.0/names'
-// $.get(queryUrl)
-//     .done(data => {
-//         console.log(data);
-//     });
+    var selector = d3.select("#selDataset");
+    // Use the list of sample names to populate the select options
+    d3.json(queryUrl).then((data) => {
+        var pokeList = data;
+        var pokeNames = [];
 
-// // get table references
-// var tbody = d3.select("tbody");
+        for (var i = 0; i < pokeList.length; i++) {
+            var pokeID = pokeList[i][0];
+            var pokeName = pokeList[i][1];
+            pokeNames.push(pokeName + "-" + pokeID);
 
-// function buildTable(data) {
-//   // First, clear out any existing data
-//   tbody.html("");
+        }
+        var pokeAlpha = pokeNames.sort();
+        // console.log(pokeNames);
+        // Grab a reference to the dropdown select element
 
-//   // Next, loop through each object in the data
-//   // and append a row and cells for each value in the row
-//   data.forEach((dataRow) => {
-//     // Append a row to the table body
-//     var row = tbody.append("tr");
 
-//     // Loop through each field in the dataRow and add
-//     // each value as a table cell (td)
-//     Object.values(dataRow).forEach((val) => {
-//       var cell = row.append("td");
-//       cell.text(val);
-//     });
-//   });
-// }
+        pokeAlpha.forEach((pokemon) => {
+            selector
+                .append("option")
+                .text(pokemon)
+                .property("value", pokemon);
+        });
 
-// buildTable(queryUrl);
+    });
+};
+
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu.property("value");
+    // Initialize an empty array for the country's data
+    var data = [];
+    var values = dataset.split("-");
+    var id_part1 = values[0];
+    var id_part2 = values[1];
+    console.log(id_part2);
+};
+init();

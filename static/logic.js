f55b819d87fae54d0ae575180d7a56aa7ccfae6c
Jessica Chester <chesterjessica10@gmail.com>
2021-05-05 20:25:34 -0500

    drop down list

--- static/logic.js
+++ static/logic.js
@@ -9,7 +9,34 @@
 //     });
 
 var queryUrl = '/api/v1/names'
-d3.json(queryUrl)
-    .then(data => {
-        console.log(data);
+
+function init() {
+
+    var selector = d3.select("#selDataset");
+    // Use the list of sample names to populate the select options
+    d3.json(queryUrl).then((data) => {
+        var pokeList = data;
+        var pokeAlpha = pokeList.sort();
+        var pokeNames = [];
+
+        for (var i = 0; i < pokeAlpha.length; i++) {
+
+            pokeNames.push(pokeAlpha[i][1]);
+
+        }
+        // console.log(pokeNames);
+        // Grab a reference to the dropdown select element
+
+        
+        pokeNames.forEach((pokemon) => {
+            selector
+                .append("option")
+                .text(pokemon)
+                .property("value", pokemon);
+        });
+
     });
+};
+
+init();
+
